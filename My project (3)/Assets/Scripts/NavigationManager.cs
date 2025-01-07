5b200989bd35dc000fb490018139a0a66551ab2e
JalterCode <jakester502@gmail.com>
2025-01-07 18:55:17 -0500

    code update but not finished

--- My project (3)/Assets/Scripts/NavigationManager.cs
+++ My project (3)/Assets/Scripts/NavigationManager.cs
@@ -1,15 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+
 using UnityEngine;
+
 using UnityEngine.AI;
 
 public class NavigationManager : MonoBehaviour
 {
 
-    public Transform startingPoint;
+    public Transform visitorHead;
 
-    public Transform endPoint;
+    private List<Transform> TargetLocations;
+    
+    public Transform targetLocationParent;
+
+    private Transform selectedTargetLocation;
+    private Transform startPoint;
 
     public LineRenderer lineRenderer;
 
@@ -20,21 +25,34 @@ public class NavigationManager : MonoBehaviour
     {
         path = new NavMeshPath();
         elapsed = 0.0f;
+
+        this.TargetLocations = new List<Transform>();
+        foreach(Transform location in this.targetLocationParent){
+            if(location != this.targetLocationParent){
+                this.TargetLocations.Add(location);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform endPoint = chosenEndPoint();
         elapsed += Time.deltaTime;
         if(elapsed > 1.0f){
             elapsed -= 1.0f;
-            NavMesh.CalculatePath(startingPoint.position, endPoint.position,NavMesh.AllAreas, path);
+            NavMesh.CalculatePath(startPoint.position, endPoint.position,NavMesh.AllAreas, path);
         }
 
         lineRenderer.positionCount=path.corners.Length;
         lineRenderer.SetPositions(path.corners);
     }
 
+    private Transform chosenEndPoint()
+    {
+        return TargetLocations[0];
+    }
 
     public void NavigateTo(GameObject go){}
 }
