2954cc62e00c02cd0ea8856e67fd81ebfd782dfe
JalterCode <jakester502@gmail.com>
2025-01-08 21:26:33 -0500

    random

---
+++ My project (3)/Assets/Scripts/AnimateMaterialOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMaterialOffset:MonoBehaviour{
+
+
+    public Material material;
+    public float speed=1;
+    private Vector2 offset=new Vector2();
+    void Update()
+    {
+        offset.x+=speed*Time.deltaTime;
+        this.material.SetTextureOffset("_MainTex",offset );
+    }
+
+}
