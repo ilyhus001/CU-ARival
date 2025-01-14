8d11e1e6dfe649b1332bdb33ab86649cb79b9e8b
firas <firaselezzi1@gmail.com>
2025-01-13 23:59:36 -0500

    Wrote script for Dropdown
    Button and Dropdown now work together

--- My project (3)/Assets/Scripts/findRoomScript.cs
+++ My project (3)/Assets/Scripts/findRoomScript.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;  // Import the SceneManager for scene loading
+using TMPro;
 
 public class findRoomScript : MonoBehaviour
 {
     public Button myButton; // Drag your button into this field in the Inspector
+    public TMP_Dropdown dropDown;
+    
+
 
     void Start()
     {
+        if (dropDown == null)
+    {
+        Debug.LogError("Dropdown is not assigned!");
+        return;
+    }
+
+    Debug.Log("Dropdown initial state: " + dropDown.gameObject.activeSelf);
         if (myButton == null)
         {
             Debug.LogError("myButton is not assigned in the Inspector!");
@@ -17,10 +28,19 @@ public class findRoomScript : MonoBehaviour
         myButton.onClick.AddListener(LoadNavMeshScene);
     }
 
+    public string getRoom()
+    {
+        // Get the index of the selected item in the dropdown
+        int selectedIndex = dropDown.value;
+        
+        // Return the text of the selected option
+        return dropDown.options[selectedIndex].text;
+    }
+
     public void LoadNavMeshScene()
     {
-        Debug.Log("Button clicked!");
-        print("HI");
+        Debug.Log("Button clicked in room!"+ getRoom());
+        print("HI" + getRoom());
 
         // Load NavScene
         SceneManager.LoadScene("NavScene");
