37894888feee7cd595fdb8be5091be3fa7ddb21c
JalterCode <jakester502@gmail.com>
2025-01-20 22:08:08 -0500

    added animation for navigation target display

--- My project (3)/Assets/Scripts/AnimationSCript.cs
+++ My project (3)/Assets/Scripts/AnimationSCript.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class AnimationSCript : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator navTarget;
+
+    
 
     public void ButtonPress(){
         animator.SetBool("ButtonPress",true);
         animator.SetBool("NavigationTargetSelected",false);
-
     }
 
     public void navigationTargetSelected(){
         animator.SetBool("NavigationTargetSelected",true);
         animator.SetBool("ButtonPress",false);
+        navTarget.SetBool("locationPressed",true);
     }
     // Start is called before the first frame update
     void Start()
