59ea854aab1617afdd15b80a4bfeb8c58d80cfe3
JalterCode <jakester502@gmail.com>
2025-01-22 20:07:00 -0500

    kinda works but model not oriented correctly

--- My project (3)/Assets/Scripts/NavigationManager.cs
+++ My project (3)/Assets/Scripts/NavigationManager.cs
@@ -2,53 +2,19 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
+using Unity.XR.CoreUtils; // For XR Origin
 
 public class NavigationManager : MonoBehaviour
 {
     public static NavigationManager instance;
-    public Transform startingPoint;
+    public Transform startingPoint; // Reference to AR Camera
     private Transform endPoint;
     public LineRenderer lineRenderer;
     float elapsed;
     public NavMeshPath path;
 
     [SerializeField] private ARTrackedImageManager trackedImageManager;
-
-
-    private void OnEnable() => trackedImageManager.trackedImagesChanged += OnChanged;
-
-    private void OnDisable() => trackedImageManager.trackedImagesChanged -= OnChanged;
-
-    private void OnChanged(ARTrackedImagesChangedEventArgs eventArgs){
-                Debug.Log("Image");
-
-        foreach (var newImage in eventArgs.added){
-            string imageName = newImage.referenceImage.name;
-            Transform targetObject = GameObject.Find(imageName).transform;
-            if(targetObject != null){
-                startingPoint.position = targetObject.transform.position;
-                startingPoint.rotation = targetObject.transform.rotation;
-                Debug.Log($"Starting point repositioned to {imageName} location");
-
-            }
-            else{
-                Debug.Log($"Starting point repositioned to {imageName} location");
-            }
-        }
-        foreach (var updatedImage in eventArgs.updated){
-            string imageName = updatedImage.referenceImage.name;
-            Transform targetObject = GameObject.Find(imageName).transform;
-            if(targetObject != null){
-                startingPoint.position = targetObject.transform.position;
-                startingPoint.rotation = targetObject.transform.rotation;
-                Debug.Log($"Starting point repositioned to {imageName} location");
-
-            }
-            else{
-                Debug.Log($"Starting point repositioned to {imageName} location");
-            }
-        }
-    }
+    private XROrigin xrOrigin; // Reference to XR Origin
 
     void Start()
     {
@@ -56,10 +22,49 @@ public class NavigationManager : MonoBehaviour
         elapsed = 0.0f;
         instance = this;
 
+        // Get reference to XR Origin
+        xrOrigin = FindObjectOfType<XROrigin>();
+        if (xrOrigin == null)
+        {
+            Debug.LogError("XR Origin not found!");
+        }
+
         GameObject roomObject = GameObject.Find(findRoomScript.GetDestination());
         if (roomObject != null)
         {
             endPoint = roomObject.transform;
+        }
+    }
+
+    private void OnEnable() => trackedImageManager.trackedImagesChanged += OnChanged;
+
+    private void OnDisable() => trackedImageManager.trackedImagesChanged -= OnChanged;
+
+    private void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        Debug.Log("Image detected");
+
+        foreach (var newImage in eventArgs.added)
+        {
+            string imageName = newImage.referenceImage.name;
+            GameObject targetObject = GameObject.Find(imageName);
+
+            if (targetObject != null)
+            {
+                // Calculate the position difference between the detected image and the target object
+                Vector3 offsetPosition = targetObject.transform.position - newImage.transform.position;
+                Quaternion offsetRotation = targetObject.transform.rotation * Quaternion.Inverse(newImage.transform.rotation);
+
+                // Manually update the XR Origin's position and rotation
+                xrOrigin.transform.position += offsetPosition;
+                xrOrigin.transform.rotation = offsetRotation;
+
+                Debug.Log($"XR Origin manually adjusted to align with {imageName} location.");
+            }
+            else
+            {
+                Debug.LogError($"Could not find object named: {imageName}");
+            }
         }
     }
 
@@ -80,7 +85,6 @@ public class NavigationManager : MonoBehaviour
 
     public void UpdateNavigationTarget(string roomName)
     {
-
         GameObject roomObject = GameObject.Find(roomName);
 
         if (roomObject != null)
